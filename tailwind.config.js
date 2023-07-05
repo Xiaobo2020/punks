f89c4c1c90638974696e8d2dfbc28cbfedee855f
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-07-05 22:18:04 +0800

    chore: add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [require("daisyui")],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
     screens: {
       // ----- default screens -----
       sm: "640px",
