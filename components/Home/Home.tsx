f947e9a07c1eb0059e07cf662639b5a6c201102d
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-07-06 09:14:39 +0800

    chore: checkbox

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import classNames from "classnames";
 import ButtonGroup from "../ButtonGroup/ButtonGroup";
 
 const SRC = {
@@ -26,6 +27,7 @@ const options = [
 
 const Home = () => {
   const [sortType, setSortType] = useState(0);
+  const [alwaysShowIds, setAlwaysShowIds] = useState(false);
   return (
     <main className="box-border flex max-w-screen-xl flex-col">
       <div className="m-2 h-[140px] w-[140px] overflow-hidden rounded-[70px] bg-[#f7931a]">
@@ -149,6 +151,39 @@ const Home = () => {
             setSortType(v);
           }}
         />
+      </div>
+      <div>
+        <div
+          className="flex flex-row flex-nowrap items-center hover:cursor-pointer"
+          onClick={() => {
+            setAlwaysShowIds(!alwaysShowIds);
+          }}
+        >
+          <span
+            className={classNames([
+              "box-content inline-block h-[14px] w-[14px] border-[1px] border-white/[.25]",
+              {
+                "!border-[#f7931a]": alwaysShowIds,
+                "bg-[#f7931a]": alwaysShowIds,
+              },
+            ])}
+          >
+            <svg
+              viewBox="0 0 64 64"
+              className={classNames([
+                "text-[#18181c]",
+                {
+                  invisible: !alwaysShowIds,
+                },
+              ])}
+            >
+              <path d="M50.42,16.76L22.34,39.45l-8.1-11.46c-1.12-1.58-3.3-1.96-4.88-0.84c-1.58,1.12-1.95,3.3-0.84,4.88l10.26,14.51  c0.56,0.79,1.42,1.31,2.38,1.45c0.16,0.02,0.32,0.03,0.48,0.03c0.8,0,1.57-0.27,2.2-0.78l30.99-25.03c1.5-1.21,1.74-3.42,0.52-4.92  C54.13,15.78,51.93,15.55,50.42,16.76z"></path>
+            </svg>
+          </span>
+          <span className="ml-3 inline-block text-sm font-normal text-white/[.82]">
+            Always show IDs
+          </span>
+        </div>
       </div>
     </main>
   );
